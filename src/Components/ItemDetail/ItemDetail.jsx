9ed283919074838da84cdf9d24feff936075e75c
Sonsoles Clouse <74992187+Malibu54@users.noreply.github.com>
2022-12-07 11:50:44 -0300

    //add ItemDetail
    No funciona

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -1,13 +1,22 @@
 import ItemCount from "../ItemCount/ItemCount";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function ItemDetail() {
+
+const [goToCart, setGoToCart] = useState(false);
+
   return (
     <div className="container">
     <div className="detail">
     <img className="detail__img" src={detail.imgurl} alt=""/>
     <div className="content">
     <h1>{detail.title}</h1>
-        <ItemCount />
+        {
+            goToCart
+            ?  <Link to="/CartWidget">Finalizar mi compra</Link>
+            : <ItemCount />
+        }
     </div>
     </div>
     </div>
