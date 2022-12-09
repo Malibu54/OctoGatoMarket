ed51b8a6e1fcee7cc44eb75adadf72c8af205b2d
Sonsoles Clouse <74992187+Malibu54@users.noreply.github.com>
2022-12-09 14:56:26 -0300

    // add
    No renderiza imagenes desde el json

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,48 +1,6 @@
 
 
 
-/* const octoCats = [
-  {
-    id: "001",
-    title: "Gato 1",
-    imgurl: "/imgs/octocat1.jpg",
-    price: "100",
-    description: "lorem ipsum dolor sit ",
-    category: "Category Cian",
-  },
-  {
-    id: "002",
-    title: "Gato 2",
-    imgurl: "/imgs/octocat2.jpg",
-    price: "200",
-    description: "lorem ipsum dolor sit ",
-    category: "Category Black",
-  },
-  {
-    id: "003",
-    title: "Gato 3",
-    imgurl: "/imgs/octocat3.jpg",
-    price: "300",
-    description: "lorem ipsum dolor sit ",
-    category: "Category Black",
-  },
-  {
-    id: "004",
-    title: "Gato 4",
-    imgurl: "/imgs/octocat4.jpg",
-    price: "400",
-    description: "lorem ipsum dolor sit ",
-    category: "Category White",
-  },
-  {
-    id: "005",
-    title: "Gato 5",
-    imgurl: "/imgs/octocat5.jpg",
-    price: "500",
-    description: "lorem ipsum dolor sit ",
-    category: "Category Black",
-  },
-]; */
 
 import data from "../mockdata";
 import { useEffect, useState } from "react";
@@ -51,7 +9,7 @@ import { useParams } from "react-router-dom";
 import "./Style.css";
 
 
-const ItemListContainer = ({ greeting }) => {
+const ItemListContainer = ({ presentation }) => {
   const [productList, setProductList] = useState([]);
   const { categoryName } = useParams(); 
   console.log(categoryName);
@@ -70,6 +28,7 @@ const ItemListContainer = ({ greeting }) => {
 
   return (
     <>
+    <h1>{presentation}</h1>
       <ItemList lista={productList} />{" "}
     </>
   );
